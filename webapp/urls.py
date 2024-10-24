6e019ff73f2be6e573f596d67fc6a1842f3b2f50
pcfatec <emailverdadeiro@email.com>
2024-10-24 16:33:25 -0300

    start auth implementation

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = "app"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("signup", views.signup_page, name="signup"),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("signup-confirmation/", views.signup_confirmation, name="signup-confirmation"),
     path("health-check", views.health_check),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.core.mail import send_mail
+from . import forms
 
 
 # Create your views here.
@@ -8,3 +10,21 @@ def index(request):
 
 def health_check(request):
     return HttpResponse("ok")
+
+
+def signup_page(request):
+    form = forms.RegistrationForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            send_mail(
+                "Confirmação de email",
+                "Codigo ai man",
+                "mockingbird@email.com",
+                [form.cleaned_data["email"]],
+            )
+            return redirect("app:signup-confirmation")
+    return render(request, "webapp/signup.html", {"form": form})
+
+
+def signup_confirmation(request):
+    return render(request, "webapp/confirmation.html", {})
