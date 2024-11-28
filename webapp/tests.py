f391678ee1d20285f12cd01d6042c4486679e72c
pcfatec <emailverdadeiro@email.com>
2024-11-28 13:52:08 -0300

    fix health-check error

--- webapp/tests.py
+++ webapp/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 # Create your tests here.
 class ViewTests(TestCase):
     def test_health_check(self):
-        response = self.client.get("/health-check")
+        response = self.client.get("/health-check/")
+        print(response)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "ok")
